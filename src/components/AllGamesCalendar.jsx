3680bf21f9ef2e27b01f46a279fdc5e8278c6c26
Tekknobot <thrillatron@hotmail.com>
2025-09-23 22:02:56 -0400

    Adjust max width.

--- src/components/AllGamesCalendar.jsx
+++ src/components/AllGamesCalendar.jsx
@@ -700,7 +700,7 @@ export default function AllGamesCalendar(){
   function openCompare(game){ setCompareGame(game); setCompareOpen(true); }
 
   return (
-    <Box sx={{ mx:'auto', width:'100%', maxWidth: 520, p:1.5 }}>
+    <Box sx={{ mx:'auto', width:'100%', maxWidth: 720, p:1.5 }}>
       {/* top header (sticky) */}
       <Box sx={{ position:'sticky', top:0, zIndex:(t)=>t.zIndex.appBar, bgcolor:'background.default', pt:1, pb:1 }}>
         <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ px:1.5 }}>
