7565f1c00070b52b0133d0fe99e65273aabdb24a
Tekknobot <thrillatron@hotmail.com>
2025-10-07 13:37:37 -0400

    Refactor.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Box } from "@mui/material";
-import AllGamesCalendar from "./components/AllGamesCalendar";
+
+import Header from "./components/Header";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Header from "./components/Header";
-import GamePage from "./components/GamePage";
+import Privacy from "./components/Privacy";   
+import Blog from "./components/Blog";         
+import All from "./components/All";           
 
-export default function App(){
+export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Box sx={{ p:2 }}>
-        <Routes>
-          <Route path="/" element={<Navigate to="/all" replace />} />
-          <Route path="/all" element={<AllGamesCalendar />} />
-          <Route path="/game/:id" element={<GamePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Box>
+      <Routes>
+        <Route path="/" element={<All />} />
+        <Route path="/all" element={<All />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy" element={<Privacy />} />  
+        <Route path="/blog" element={<Blog />} />        
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </BrowserRouter>
   );
 }
